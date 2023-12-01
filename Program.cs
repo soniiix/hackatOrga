4af97ecb8aae6293e1a2f5d9f9208bc35f6a5b17
qcouzinet <qcouzinet@B307DELL08>
2023-12-01 09:21:31 +0100

    test

--- Program.cs
+++ Program.cs
@@ -2,14 +2,10 @@ namespace hackatOrga
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+            //--- Lancement du formulaire ---
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
