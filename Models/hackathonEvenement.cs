252ee3261f3f48d1cc942d82ba66d71a57108162
Naïm DEHAS <naim.dehas.nd@gmail.com>
2023-12-19 15:02:43 +0100

    Mapping objet

---
+++ Models/hackathonEvenement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace hackatOrga.Models
+{
+    [Table("hackathon_evenement")]
+    public class HackathonEvenement
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [ForeignKey(nameof(Hackathon))]
+        public int hackathon_id { get; set; }
+        [ForeignKey(nameof(Evenement))]
+        public int evenement_id { get; set; }
+
+        public virtual Hackathon Hackathon { get; set; }
+        public virtual Evenement Evenement{ get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using hackatOrga.Models;    //// <--- remplacer xxx par le nom de votre projet
+
 namespace hackatOrga
 {
     internal static class Program
@@ -5,6 +8,16 @@ namespace hackatOrga
         [STAThread]
         static void Main()
         {
+            //connexion à la BD
+            MonDbContext cnx = new MonDbContext();
+
+            InscriptionHackathon uneInscriptionHackathon = new InscriptionHackathon();
+            uneInscriptionHackathon = cnx.InscriptionHackathons.Find(3);   //album 50 chargé
+
+            System.Diagnostics.Debug.WriteLine(uneInscriptionHackathon.date);   //Ok
+            Participant unParticipant = uneInscriptionHackathon.Participant;                   //On veut l'artiste de l'album
+            System.Diagnostics.Debug.WriteLine(unParticipant.nom);  //plante : l'artiste n'est pas chargé
+
             //--- Lancement du formulaire ---
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
